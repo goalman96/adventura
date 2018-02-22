0ecab2a6f7ff3b54b19c422d95abd71f45efa02b
vont02 <vont02@sb105h21.ad.vse.cz>
2018-02-22 17:35:02 +0100

    úvodní commit adventury

---
+++ src/main/java/com/github/goalman96/adventura/App.java
@@ -0,0 +1,13 @@
+package com.github.goalman96.adventura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
